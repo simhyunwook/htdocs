f40bdd98c9957f63ece827e84bdd85d6addc499d
simhyunwook <ssssmuk@naver.com>
2016-09-01 10:40:37 +0900

    첫 php수업

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-	phpinfo();
+//	phpinfo();
 ?>
 <!DOCTYPE html>
 <html lang="ko">
@@ -14,7 +14,17 @@
 		<?php
 			print"안녕 나는 오늘 기분이 별로 안좋으니깐 조심해 경식아";
 		?>
-		<li>3번</li>
+		<li>3번째 예제<?php
+			print PHP_VERSION."<br>";
+			print PHP_OS."<br>";
+			define("HELLO","안녕하세요");
+			print HELLO."<br>";
+			define("_18","신발");
+			print _18."<br>";
+			print __DIR__."<br>";
+		?>
+		</li>
+		
 		<li>4번</li>
 	</ul>
 </body>
