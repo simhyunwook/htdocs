f5b9c71288f60c60d606d5fd7b7025581c47a1ec
simhyunwook <ssssmuk@naver.com>
2016-09-01 10:25:36 +0900

    첫 php수업

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+	phpinfo();
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<title>오늘은 첫번재 php수업</title>
+</head>
+<body>
+	<ul>
+		<li>1번</li>
+		<li>2번</li>
+		<?php
+			print"안녕 나는 오늘 기분이 별로 안좋으니깐 조심해 경식아";
+		?>
+		<li>3번</li>
+		<li>4번</li>
+	</ul>
+</body>
+</html>
