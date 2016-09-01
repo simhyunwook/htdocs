4b8d64f380cb00c2539920d3127ee64d61ba3133
simhyunwook <ssssmuk@naver.com>
2016-09-01 11:18:40 +0900

    첫 php수업

--- test.php
+++ test.php
@@ -26,6 +26,32 @@
 		</li>
 		
 		<li>4번</li>
+		<?php
+			$data="안녕하세요!!";
+			$data1=1;
+			print $data."<br>";
+			print $data1; // 자동으로 타입 변환됩니다.
+			$week[]="월";
+			$week[]="화";
+			//
+			$week[0]="월";
+			$week[1]="화";
+			print $week[0];
+			print $week[1];
+			//array 함수 사용하기
+			$month=array(1,2,3,4,5,6,7,8,9,"월","월월");
+			//php 5.4 이상 배열 []사용 가능
+			$year=[2016,2017];
+			//배열 초기화
+			$month=array();
+			//연관배열
+			$member["name"]="심현욱";
+			$member["age"]=18;
+			$member["height"]=176;
+			print $member["age"];
+			print "<pre>";
+			print_r ($member);
+		?>
 	</ul>
 </body>
 </html>
