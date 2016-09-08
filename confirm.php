6e5277ce73518c9f852f7ae4cec2255c5b07531b
simhyunwook <ssssmuk@naver.com>
2016-09-08 11:32:19 +0900

    3번째php

---
+++ confirm.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+$_SESSION["name"]=$_POST["name"];
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
