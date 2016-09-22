bbec6552e0073cb75e2cec94068e9b593b6043bc
simhyunwook <ssssmuk@naver.com>
2016-09-22 11:43:45 +0900

    20160922

--- kccdb.php
+++ kccdb.php
@@ -1,6 +1,7 @@
 <?php
 try{
-$pdo=new PDO('mysql:host=localhost;dbname=kcc1;charset=utf8','root','');
+$pdo=new PDO('mysql:host=localhost;dbname=kcc;charset=utf8','root','');
+$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
         print "접속성공";
 }
--- list.php
+++ list.php
@@ -1,11 +1,12 @@
 <?php//웹에서 써서 PHP로 보내기
-$search='%'.$_POST['search'].'%';
 include 'kccdb.php';
-$SQL="select * from membe where last_name like:last_name or first_name like:first_name";
-$stmh=$pdo->prepare($SQL);
-$stmh->bindvalue(':last_name',$search,PDO::PARAM_STR);
+$search='%'.$_POST['search'].'%';
+//print $search;
+$sql="select * from membe where last_name like :last_name or first_name like :first_name";
+$stmh=$pdo->prepare($sql);
+$stmh->bindValue(':last_name',$search,PDO::PARAM_STR);
 $stmh->bindvalue(':first_name',$search,PDO::PARAM_STR);
-$stmh->execte();
-$count=$stmch->rowcount();
+$stmh->execute();
+$count=$stmh->rowCount();
 print $count."건 입니다";
 ?>
