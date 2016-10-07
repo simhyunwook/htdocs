ce02a58c327790eb9df3aa37374d9d53c6c59725
simhyunwook <ssssmuk@naver.com>
2016-10-07 11:32:31 +0900

    20161007

--- kcc_print.php
+++ kcc_print.php
@@ -7,28 +7,28 @@
 <body>
     <?php
     try{
-    $pdo = new PDO("mysql:host=127.0.0.1;dbname=kcc;charset=utf8","root", "");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+    $db = new PDO("mysql:host=127.0.0.1;dbname=kcc;charset=utf8","root", "");
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
     }
     catch(PDOEXCEPTION $Exception){
         die("연결실패".$Exception->getMessage());
     }
     //$homen = preg_replace("/(^02.{0}|^01.{1}|[0-9]{3})([0-9]+)([0-9]{4})/", "$1-$2-$3", $homen);
     $sql = "INSERT INTO member (korea_name,'english_name','year','month','day','id','password','homephone','selphone','sms','home_post') VALUE (:korea_name,:english_name,:year,:month,:day,:id,:password,:homephone,:selphone,:sms,:home_post)";
-    $rs = $pdo->prepare($sql);
-    $rs->bindValue(':korea_name','$_POST["korea_name"]');
-    $rs->bindValue(':english_name','$_POST["english_name"]');
-    $rs->bindValue(':year','$_POST["year"]');
-    $rs->bindValue(':month','$_POST["month"]');
-    $rs->bindValue(':day','$_POST["day"]');
-    $rs->bindValue(':id','$_POST["id"]');
-    $rs->bindValue(':password','$_POST["password"]');
-    $rs->bindValue(':homephone','$_POST["homephone"]');
-    $rs->bindValue(':selphone','$_POST["selphone"]');
-    $rs->bindValue(':sms','$_POST["sms"]');
-    $rs->bindValue(':home_post','$_POST["home_post"]');
-    $rs->execute();/* or die("<script>alert('실패')</script>");*/
+    $prepare = $db->prepare($sql);
+    $prepare->bindValue(':korea_name','$_POST["korea_name"]');
+    $prepare->bindValue(':english_name','$_POST["english_name"]');
+    $prepare->bindValue(':year','$_POST["year"]');
+    $prepare->bindValue(':month','$_POST["month"]');
+    $prepare->bindValue(':day','$_POST["day"]');
+    $prepare->bindValue(':id','$_POST["id"]');
+    $prepare->bindValue(':password','$_POST["password"]');
+    $prepare->bindValue(':homephone','$_POST["homephone"]');
+    $prepare->bindValue(':selphone','$_POST["selphone"]');
+    $prepare->bindValue(':sms','$_POST["sms"]');
+    $prepare->bindValue(':home_post','$_POST["home_post"]');
+    $prepare->execute();/* or die("<script>alert('실패')</script>");*/
     print "INSERT 성공";
     //echo $sql."<br>"; 
     echo "이름 : ".$_POST["korea_name"]."<br>";
