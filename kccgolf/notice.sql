01e153954052584cd79e390de484b4af813383eb
simhyunwook <ssssmuk@naver.com>
2016-10-21 11:39:29 +0900

    kcc

---
+++ kccgolf/notice.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- 생성 시간: 16-10-21 04:26
+-- 서버 버전: 10.1.16-MariaDB
+-- PHP 버전: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `kcc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `notice`
+--
+
+CREATE TABLE `notice` (
+  `idx` int(100) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `day` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
